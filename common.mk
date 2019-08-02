7123779cb058392f1c9e68b8072a19c9763b31d0
Yichi Chen <yichichen@google.com>
2019-08-01 17:50:50 -0700

    sm8150-common: enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to
    HWC queue stuffing
    
    Bug: 138601429
    Test: 1. Open Camera
          2. Take some pictures
          3. Swipe vertically to go back to home screen
          4. Measure the "FrameMissed" count from systrace result
    
    Change-Id: I1392ae833f246351bdb8398e6e63e2e787f41af2

--- common.mk
+++ common.mk
@@ -198,6 +198,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# Enable backpressure for GL comp
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_gl_backpressure=1
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
