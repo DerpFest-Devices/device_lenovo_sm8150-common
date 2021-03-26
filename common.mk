13a57341992f280c585e6797adcca6dc35539195
Lucchetto <lucchetto.tie@live.com>
2021-03-26 12:20:01 +0100

    sm8150-common: build sensors multi hal 2
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I2e9da4885c070cbb6a1d9882e8b51a05a7a80f45

--- common.mk
+++ common.mk
@@ -382,6 +382,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
     libsensorndkbridge \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
