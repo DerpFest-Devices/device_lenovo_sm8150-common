0d8e85cb5f383d9c3cbd73b3063985811456e45d
chematelegram <chfondia@gmail.com>
2021-05-21 20:15:18 +0200

    Revert "sm8150-common: livedisplay: Uprev to 2.1"
    This reverts commit 7c1a4f8b6d30ef95ba0e6534dc2d6eca8b62b561.

--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -14,12 +14,12 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.livedisplay@2.1-service.lenovo_msmnile"
+#define LOG_TAG "vendor.lineage.livedisplay@2.0-service.lenovo_msmnile"
 
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
-#include <vendor/lineage/livedisplay/2.1/IPictureAdjustment.h>
+#include <vendor/lineage/livedisplay/2.0/IPictureAdjustment.h>
 
 #include "AntiFlicker.h"
 #include "SunlightEnhancement.h"
@@ -29,11 +29,11 @@ using android::OK;
 using android::sp;
 using android::status_t;
 
+using ::vendor::lineage::livedisplay::V2_0::ISunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_0::implementation::SunlightEnhancement;
 using ::vendor::lineage::livedisplay::V2_0::sdm::SDMController;
-using ::vendor::lineage::livedisplay::V2_1::IAntiFlicker;
-using ::vendor::lineage::livedisplay::V2_1::ISunlightEnhancement;
-using ::vendor::lineage::livedisplay::V2_1::implementation::AntiFlicker;
-using ::vendor::lineage::livedisplay::V2_1::implementation::SunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_0::IAntiFlicker;
+using ::vendor::lineage::livedisplay::V2_0::implementation::AntiFlicker;
 
 int main() {
     status_t status = OK;
