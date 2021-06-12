8dd856d44466d6ca9bfc8a59bc91fc334d27df73
chematelegram <chfondia@gmail.com>
2021-06-12 15:15:28 +0200

    sm8150-common: Drop LiveDisplay

--- common.mk
+++ common.mk
@@ -282,8 +282,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.lenovo_msmnile
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_PACKAGES += \
