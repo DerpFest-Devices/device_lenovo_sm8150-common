417f2ad17f1332ead2686a38a53922dfe36d9896
chematelegram <chfondia@gmail.com>
2021-06-17 23:50:44 +0200

    sm8150-common: Adapt overlays for Spark
    -Update to overlay-spark
    -Remove Smart Pixels
    -Move FOD config to spark_config.xml
    -Move Smart Charging to spark_config.xml
    -Move Pocket Lock to spark_config.xml
    -Enable face auth only when swiping security view
    -Add FPS info

--- common.mk
+++ common.mk
@@ -25,12 +25,12 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-spark
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay-spark/lineage-sdk \
+    $(LOCAL_PATH)/overlay-spark/packages/apps/Snap
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
