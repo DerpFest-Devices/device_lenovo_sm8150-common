165ea01c240e776d3e70c904bf0447629a79a1e2
chematelegram <chfondia@gmail.com>
2021-06-26 07:28:26 +0200

    sm8150-common: Bring back LiveDisplay
    This reverts commit 2e49f9626ac19ba7e6eefbc5f90417598f3ebdaa.

--- common.mk
+++ common.mk
@@ -283,7 +283,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.1-service.lenovo_msmnile
 
 # Media
 PRODUCT_PACKAGES += \
