35264234d0fa04ec7fd0b5b9b0a70ef7022b25ee
Lucchetto <lucchetto.tie@live.com>
2021-10-12 20:48:48 +0200

    [DNM] sm8150-common: cleanup sepolicy
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I2247ca39b12ba0fd8170563045bfcd1e902df364

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,6 +173,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH_COMMON)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH_COMMON)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
