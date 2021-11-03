LineageOS/android_device_lenovo_sm8150-common

sm8150-common: Decommonize snap
on heart, snap keeps defaulting the front camera to the IR camera where-as
other camera apps correctly select the proper selfie camera

Change-Id: I2c60ee2b0af4b1114d5a79c9a809f543cde984e9

common.mk
@@ -29,8 +29,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
@@ -142,8 +141,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     libdng_sdk \
     libgui_vendor \
-    libxml2 \
-    Snap
+    libxml2
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so

DerpFest-Devices/device_lenovo_sm8150-common

common.mk
@@ -29,8 +29,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-spark/lineage-sdk \
-    $(LOCAL_PATH)/overlay-spark/packages/apps/Snap
+    $(LOCAL_PATH)/overlay-spark/lineage-sdk
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
@@ -146,8 +145,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk \
     libgui_vendor \
     libqti-perfd-client \
-    libxml2 \
-    Snap
+    libxml2
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so