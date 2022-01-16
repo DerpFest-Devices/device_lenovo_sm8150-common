4293fe839c2ac005b3f44a27b3ef363d2bf5ff0b
TheScarastic <warabhishek@gmail.com>
2022-01-16 03:31:04 +0100

    sm8150-common: parts: Adapt to S style

--- parts/src/org/lineageos/settings/doze/DozeUtils.java
+++ parts/src/org/lineageos/settings/doze/DozeUtils.java
@@ -47,6 +47,7 @@ public final class DozeUtils {
     protected static final String GESTURE_SMART_WAKE_KEY = "gesture_smart_wake";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
+    protected static final String DOZE_ENABLE = "doze_enable";
 
     public static void startService(Context context) {
         if (DEBUG) Log.d(TAG, "Starting service");
