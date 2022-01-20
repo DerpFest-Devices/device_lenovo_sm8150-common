a8695846a76d78c32e4a26c3355c7054e5eb7318
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2022-01-20 19:16:08 +0100

    sm8150-common: Ignore Selinux Neverallows for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,6 +165,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmnile
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+SELINUX_IGNORE_NEVERALLOWS := true
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH_COMMON)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH_COMMON)/sepolicy/public
