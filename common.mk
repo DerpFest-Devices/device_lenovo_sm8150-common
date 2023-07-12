21b4d825141a5cefb0a41db344149c5b5d79e0f7
adi8900 <adrianszymanski242@gmail.com>
2023-07-12 21:07:52 +0200

    Revert "sm8150-common: Switch to AOSP WFD"
    Reason of revert: AOSP WFD sucks.
    
    This reverts commit a3a74c5deb0dc1f04c8231d53fb750b956f9ac8f.

--- common.mk
+++ common.mk
@@ -184,6 +184,11 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     gralloc.msmnile \
     hwcomposer.msmnile \
+    libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
+    libqdMetaData \
+    libqdMetaData.system \
+    libqdutils \
     libtinyxml \
     libvulkan \
     memtrack.msmnile \
@@ -442,3 +447,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl \
+    libwfdaac_vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
