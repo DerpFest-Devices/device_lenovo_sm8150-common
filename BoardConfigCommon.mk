4836ffb7ac8305da2aeba42b875e7391035cec6a
Michael Bestas <mkbestas@lineageos.org>
2024-06-19 12:07:00 +0200

    sm8150-common: Include lineage FCM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,7 +114,8 @@ TARGET_USES_FOD_ZPOS := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/derp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH_COMMON)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH_COMMON)/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH_COMMON)/manifest-qva.xml
