2f927dd260e08a621e54c890ec1360961d7b0adc
Bruno Martins <bgcngm@gmail.com>
2024-06-19 12:04:11 +0200

    sm8150-common: Inherit from QTI FCM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,6 +113,8 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH_COMMON):libudfps_extension.le
 TARGET_USES_FOD_ZPOS := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH_COMMON)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH_COMMON)/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH_COMMON)/manifest-qva.xml
