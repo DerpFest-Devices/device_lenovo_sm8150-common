99d303b96c8258e996b063fd6b0dd3f95214f6f5
chematelegram <chfondia@gmail.com>
2024-10-27 14:47:03 +0100

    sm8510-common: Remove assertions on TrustZone version
    Change-Id: Iace7117a76ab4b1d35c5691b9038cc3312246b90

--- releasetools.py
+++ releasetools.py
@@ -15,35 +15,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import hashlib
 import common
 import re
-
-def FullOTA_Assertions(info):
-  AddModemAssertion(info, info.input_zip)
-  return
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
   return
 
-def IncrementalOTA_Assertions(info):
-  AddModemAssertion(info, info.target_zip)
-  return
-
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
   return
-
-def AddModemAssertion(info, input_zip):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info.decode('utf-8'))
-  if m:
-    timestamp, firmware_version = m.group(1).rstrip().split(',')
-    if ((len(timestamp) and '*' not in timestamp) and \
-        (len(firmware_version) and '*' not in firmware_version)):
-      cmd = 'assert(lenovo.verify_modem("{}") == "1" || abort("ERROR: This package requires firmware from ZUI {} build or newer. Please upgrade firmware and retry!"););'
-      info.script.AppendExtra(cmd.format(timestamp, firmware_version))
 
 def AddImage(info, basename, dest):
   name = basename
