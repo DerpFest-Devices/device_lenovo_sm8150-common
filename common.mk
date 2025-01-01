b273369c5cc2e9725fde2d650dba6b68e03c3ed2
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:42:57 +0200

    sm8150-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3

--- common.mk
+++ common.mk
@@ -191,8 +191,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    gralloc.msmnile \
-    hwcomposer.msmnile \
+    hwcomposer.qcom \
     libion.vendor \
     libsdmcore \
     libsdmutils \
