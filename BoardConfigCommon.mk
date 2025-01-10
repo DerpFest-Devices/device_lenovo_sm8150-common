f4589f6a3e2c661ed2a93e858618f01d0abf45b0
Einar Gednochsson <pete80pro.pb@gmail.com>
2025-01-10 09:01:17 +0100

    sm8150-common: adapt for derpfest

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,7 +109,7 @@ TARGET_USES_FOD_ZPOS := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH_COMMON)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/derp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH_COMMON)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH_COMMON)/manifest-qva.xml
